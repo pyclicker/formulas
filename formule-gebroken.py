6c7fe8859d8de6018f04cb0c78719ea55f677642
Clicky <106909784+pyclicker@users.noreply.github.com>
2023-03-24 16:54:29 +0100

    Update formule-gebroken.py
    made it visual in the terminal

--- formule-gebroken.py
+++ formule-gebroken.py
@@ -1,28 +1,34 @@
 import numpy as np
 from math import *
+from tabel import raster as tabel
+
 for a in ['matplotlib.pyplot as plt']:
     exec(f'import {a}')
+
+
 class formules():
-    def __init__(self,start=0,stop=1,steps=1):
+    def __init__(self, start=0, stop=1, steps=1, formula=input('f(x) = ')):
         self.start = float(start)
         self.stop = float(stop)
         self.steps = float(steps)
-        self.stop+=self.steps
-        formule = input('f(x) = ')
+        self.stop += self.steps
+        self.formula = formula
         self.dict = {}
-        for a in list(np.arange(step=self.steps,start=self.start,stop=self.stop)):
-            formule2 = formule[::1]
-            exec(f'self.y = {formule2.replace("x",str(a))}')
+        for a in list(np.arange(step=self.steps, start=self.start, stop=self.stop)):
+            formula2 = self.formula[::1]
+            exec(f'try:\n   self.y = {formula2.replace("x", str(a))}\nexcept:\n   self.y = inf')
             self.dict[float(a)] = float(self.y)
+
     def get(self):
         return self.dict
-    
+
+
 if __name__ == '__main__':
-    coords = formules(input('start = '),input('stop = '),steps=input('steps = ')).get()
-    if input('Do you want a plot on your screen? Y/N  ') == 'y':
-        print('you typed Y')
-        plt.plot(np.array(**coords.keys),np.array(**coords.values).reshape("inf",None))
-        plt.show()
-    print(coords)
-
-
+    coords = formules(start=input('start = '), stop=input('stop = '), steps=input('steps = ')).get()
+    first = list(coords.keys())
+    first.insert(0, 'y ')
+    second = list(coords.values())
+    second.insert(0, 'x ')
+    tb = tabel(first, '|')
+    tb.append(second)
+    tb.build()
