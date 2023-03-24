03eae93a096f2e80338d8bf48c0b265ac7cc14dd
Clicky <106909784+pyclicker@users.noreply.github.com>
2023-03-24 17:04:10 +0100

    removed a usless import

--- formule-gebroken.py
+++ formule-gebroken.py
@@ -1,11 +1,6 @@
 import numpy as np
 from math import *
 from tabel import raster as tabel
-
-for a in ['matplotlib.pyplot as plt']:
-    exec(f'import {a}')
-
-
 class formules():
     def __init__(self, start=0, stop=1, steps=1, formula=input('f(x) = ')):
         self.start = float(start)
@@ -18,11 +13,8 @@ class formules():
             formula2 = self.formula[::1]
             exec(f'try:\n   self.y = {formula2.replace("x", str(a))}\nexcept:\n   self.y = inf')
             self.dict[float(a)] = float(self.y)
-
     def get(self):
         return self.dict
-
-
 if __name__ == '__main__':
     coords = formules(start=input('start = '), stop=input('stop = '), steps=input('steps = ')).get()
     first = list(coords.keys())
